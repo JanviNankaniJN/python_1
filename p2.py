87b074324367d4878431b4cc3f06a50f9a1070fe
JanviNankaniJN <janvinankani57@gmail.com>
2025-11-07 12:17:13 +0530

    first python practice

---
+++ p2.py
@@ -0,0 +1,7 @@
+#Write a program to display sum of two complex numbers.
+x=complex(3,4)
+y=complex(4,5)
+print("complex number of x:",x)
+print("complex number of y:",y)
+sum=x+y
+print("sum of complex number=",sum)
